404387f6233ae07d96a3a256d22bc95b22559c06
AGARWAL Prerita <s1959267@ed.ac.uk>
2020-04-15 20:37:04 +0100

    HPC Job Script
    WRF_Run ERA5_data

---
+++ Eddie_Job_Scripts/mpi_wrf_test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+##########################################
+#                                        #
+# This job is 4th attempt to run WRF     #
+#  with ERA5 Data                        #
+##########################################
+ 
+# Grid Engine options
+#$ -N WRF_Attempt4
+#$ -cwd
+#$ -pe mpi 16
+#$ -l h_rt=00:05:00
+#$ -M yourUUN@ed.ac.uk
+#$ -m beas
+#$ -l h_rt=24:00:00
+#$ -R y 
+# Initialise the modules framework and load required modules
+. /etc/profile.d/modules.sh
+ 
+# Load openmpi module
+module load openmpi/1.10.1
+ 
+# Run the program
+mpirun -np 4 ./wrf.exe
