923c62a57208920ea2493fd99700d96cda6d417d
AGARWAL Prerita <s1959267@ed.ac.uk>
2020-04-16 01:38:22 +0100

    Eddie modules

---
+++ setup_wrf.sh
@@ -0,0 +1,5 @@
+module load geos/compiler/intel_without_impi/2016
+module load geos/compiler/intel_openmpi/openmpi_1.10.1 
+#   not currently doing parallel netcdf module load geos/libs/netCDF4/intel-openmpi/2016
+#   nor are we using geos/libs/netCDF4/intel/2016 
+# due to following https://www2.mmm.ucar.edu/wrf/OnLineTutorial/compilation_tutorial.php#STEP1
